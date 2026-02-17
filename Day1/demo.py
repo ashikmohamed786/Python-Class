53c4011d836c37ecf715eb1349c644d9e5ded00a
ashikmohamed786 <ashikmohamed7866@gmail.com>
2026-02-17 10:10:56 +0530

    Add files via upload

---
+++ Day1/demo.py
@@ -0,0 +1,107 @@
+# apple123=10
+# print(apple123)
+# Data Type
+# int
+# float
+# str
+# bool
+
+##########
+
+# operators
+
+# birth=2006
+# future=2056
+# age=future-birth
+# print(age)
+
+# current=20
+# max=85
+# amount=500
+# rest=max-current
+# total=rest*365*amount
+# print(rest)
+# print(total)
+
+# age=20
+# max=85
+# water=2
+# years=max-age
+# days=years*365
+# rest=days*water
+# print(years)
+# print(days)
+# print(rest)
+
+# trip=1500
+# mileage=15
+# petrol=100
+# need=trip/mileage
+# fuel=petrol*need
+# print(need)
+# print(fuel)
+
+# gb=2.5
+# days=30
+# total=days*gb
+# print(total)
+
+# current=20
+# days=current*365
+# print(days)
+
+# orginalprice=1000
+# discount=40
+# decimal=discount/100
+# amount=decimal*orginalprice
+# sub=orginalprice-amount
+# print(decimal)
+# print(amount)
+# print(sub)
+
+##########
+
+#loop
+
+# num1=20
+# num2=30
+# if(num1==num2):
+#     print("num1 and num2 is equal")
+# elif(num1>num2):
+#     print("num1 is greater than num2")
+# else:
+#     print("num1 is less than num2")
+
+# x=input("enter a character:")
+# if(x=="a,e,i,o,u"):
+#     print("vowel")
+# elif(x!="a,e,i,o,u"):
+#     print("consonants")
+# else:
+#     print("not a alphabet")
+
+# x=int(input("enter mark:"))
+# if(x==100):
+#     print("o grade")
+# elif(x>=90 and x<=99):
+#     print("a grade")
+# elif(x>=80 and x<=89):
+#     print("b grade")
+# elif(x>=70 and x<=79):
+#     print("c grade")
+# elif(x>=60 and x<=69):
+#     print("d grade")        
+# elif(x>=50 and x<=59):
+#     print("e grade")
+# else:
+#     print("f grade")    
+
+
+x=int(input("enter the number"))
+y=int(input("enter the number"))
+profit=y-x
+loss=x-y
+if(x<y):
+    print("t is profit of rs.",profit)
+else:
+    print("it is loss of rs.",loss)    
